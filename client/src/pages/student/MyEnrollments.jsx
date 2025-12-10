f77959f80087fab1e47c0e68b0f338477a899196
nghiaphan04 <nghiaphan04dev@gmail.com>
2025-12-11 01:49:04 +0700

    delete reset progress action

--- client/src/pages/student/MyEnrollments.jsx
+++ client/src/pages/student/MyEnrollments.jsx
@@ -1117,16 +1117,7 @@ const MyEnrollments = () => {
                                                         {loadingSimpleCert[course._id] ? 'Loading...' : 'View Course Progress'}
                                                     </button>
                                                 )}
-                                                    <button
-                                                        onClick={() => handleResetProgress(course._id)}
-                                                        disabled={updatingProgress[course._id]}
-                                                        className="btn btn-danger px-3 py-1.5 text-sm bg-gradient-to-r from-red-500 to-red-600 text-white rounded shadow-sm hover:from-red-600 hover:to-red-700 hover:shadow transition-all duration-200 font-medium disabled:from-gray-400 disabled:to-gray-500 disabled:shadow-none disabled:opacity-70 border-0 flex items-center gap-1.5"
-                                                    >
-                                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                                                            <path fillRule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clipRule="evenodd" />
-                                                        </svg>
-                                                        {updatingProgress[course._id] ? 'Resetting...' : 'Reset Progress'}
-                                                    </button>
+                                                
                                             </div>
                                         </td>
                                     </tr>
